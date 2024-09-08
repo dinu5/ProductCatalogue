ff33cb284458008cd6de8897567d50bd0b46e399
dinu5 <cdinobandhu5@gmail.com>
2024-09-09 01:28:10 +0530

    added create product testcase

--- src/test/java/com/dino/productcatalogue/controllers/MvcControllerTest.java
+++ src/test/java/com/dino/productcatalogue/controllers/MvcControllerTest.java
@@ -3,18 +3,22 @@ package com.dino.productcatalogue.controllers;
 import com.dino.productcatalogue.dtos.ProductDto;
 import com.dino.productcatalogue.models.Product;
 import com.dino.productcatalogue.services.IProductService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -85,5 +89,24 @@ public class MvcControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(objectMapper.writeValueAsString(productList)));
 
+    }
+
+    @Test
+    public void Test_CreateProduct_CreatedSuccessfully() throws Exception {
+        Product product1 = new Product();
+        product1.setId(1L);
+        product1.setTitle("iPhone13");
+
+        ProductDto productDto = new ProductDto();
+        productDto.setId(1L);
+        productDto.setTitle("iPhone13");
+
+        when(productService.createProduct(any(Product.class))).thenReturn(product1);
+
+        mockMvc.perform(post("/products")
+                .content(objectMapper.writeValueAsString(productDto))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+//                .andExpect(content().string(objectMapper.writeValueAsString(productDto)));
     }
 }
